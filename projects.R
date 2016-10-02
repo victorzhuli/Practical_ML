d9476be1f9e9fdbee2fd95ee360cf454c1f8bdc4
victorzhuli <zhulivictor@gmail.com>
2016-10-01 22:15:48 -0400

    Update projects.R

--- projects.R
+++ projects.R
@@ -4,6 +4,7 @@ install.packages("caret")
 install.packages("rattle")
 install.packages("rpart.plot")
 install.packages("randomForest")
+install.packages('e1071', dependencies=TRUE)
 library(caret)
 library(rpart)
 library(rpart.plot)
@@ -23,6 +24,29 @@ features <- names(dt_testing[,colSums(is.na(dt_testing)) == 0])[8:59]
 dt_training <- dt_training[,c(features,"classe")]
 dt_testing <- dt_testing[,c(features,"problem_id")]
 
+# partitioning the data
+set.seed(12345)
+
+inTrain <- createDataPartition(dt_training$classe, p=0.6, list=FALSE)
+training <- dt_training[inTrain,]
+testing <- dt_training[-inTrain,]
+
+dim(training); dim(testing);
+
 # decision tree model
 modFitDT <- rpart(classe ~ ., data = training, method="class")
 fancyRpartPlot(modFitDT)
+set.seed(12345)
+
+prediction <- predict(modFitDT, testing, type = "class")
+confusionMatrix(prediction, testing$class)
+
+# random forest model
+set.seed(12345)
+modFitRF <- randomForest(classe ~ ., data = training, ntree = 1000)
+
+predictionDT <- predict(modFitDT, dt_testing, type = "class")
+predictionDT
+
+predictionRF <- predict(modFitRF, dt_testing, type = "class")
+predictionRF
